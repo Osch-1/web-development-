d9eeba5e5ab6653f2a8b35f36369192ab96cb12b
Daniil <notdedinside@users.noreply.github.com>
2020-02-29 02:22:23 +0300

    added task1 from third lab

---
+++ lw3/removeExtraBlanks.php
@@ -0,0 +1,8 @@
+<?php
+header("Content-Type: text/plain");
+
+if (isset($_GET["text"])) {
+   $_GET["text"] = trim($_GET["text"]);
+   $_GET["text"] = preg_replace("/\s+/",  " ", $_GET["text"]);
+   echo $_GET["text"];
+}
